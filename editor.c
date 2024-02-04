65667196935ce77995314a8cdb2cee28caadbbe3
Ilya Kupriyanov <ikuprianov423@gmail.com>
2024-02-04 15:54:04 +0300

    Added appending buffer, abAppend and abFree functions

--- editor.c
+++ editor.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 #include <errno.h>
 #include <sys/ioctl.h>
+#include <string.h>
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
@@ -96,6 +97,31 @@ int getWindowSize(int *rows, int *cols)
 
         return 0;
     }
+}
+
+/* *** Appending buffer *** */
+
+struct abuf {
+    char *b;
+    int len
+};
+
+#define ABUF_INIT {NULL, 0};
+
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL) return;
+        
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab)
+{
+    free(ab->b);
 }
 
 /* *** Output *** */
