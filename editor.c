70b19424233c3ff6a4c0b358487685995cbaaed2
Ilya Kupriyanov <ikuprianov423@gmail.com>
2024-02-08 17:18:50 +0300

    Added moving right at the end and left at the start of the line

--- editor.c
+++ editor.c
@@ -332,6 +332,9 @@ void editorMoveCursor(int key)
         case ARROW_LEFT:
             if (E.cx != 0) {
                 E.cx--;
+            } else if (E.cx == 0) {
+                E.cy--;
+                E.cx = E.row[E.cy].size;
             }
             break;
         case ARROW_DOWN:
@@ -342,6 +345,9 @@ void editorMoveCursor(int key)
         case ARROW_RIGHT:
             if (row && E.cx < row->size) {
                 E.cx++;
+            } else if (row && E.cx == row->size) {
+                E.cy++;
+                E.cx = 0;
             }
             break;
     }
