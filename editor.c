4c5d4931541f3ae4912962fbe13157b1a0efd148
Ilya Kupriyanov <ikuprianov423@gmail.com>
2024-07-31 10:49:58 +0300

    Added restoring highlighting after search

--- editor.c
+++ editor.c
@@ -492,6 +492,15 @@ void editorFindCallback(char *query, int key)
 	static int last_match = -1;
 	static int direction = 1;
 
+	static int saved_hl_line;
+	static char *saved_hl = NULL;
+
+	if (saved_hl) {
+		memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].render_size);
+		free(saved_hl);
+		saved_hl = NULL;
+	}
+
 	if (key == '\r' || key == '\x1b') {
 		last_match = -1;
 		direction = -1;
@@ -528,6 +537,9 @@ void editorFindCallback(char *query, int key)
 			E.cx = editorRowRxToCx(row, match - row->render);
 			E.row_offset = E.num_rows;
 
+			saved_hl_line = current;
+			saved_hl = malloc(row->render_size);
+			memcpy(saved_hl, row->hl, row->render_size);
 			memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
 			break;
 		}
