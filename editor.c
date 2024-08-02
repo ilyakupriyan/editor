a65a67f6d8dc64787e9734ec4cc296d972eedb09
Ilya Kupriyanov <ikuprianov423@gmail.com>
2024-08-02 11:39:37 +0300

    Added printing nonprintable characters

--- editor.c
+++ editor.c
@@ -805,7 +805,17 @@ void editorDrawRows(struct abuf_s *bf)
 			int current_color = -1;
 			int j;
 			for (j = 0; j < len; j++) {
-				if (hl[j] == HL_NORMAL) {
+				if (iscntlr(j)) {
+					char sym = (c[j] <= 26) ? '@' + c[j] : '?';
+					abAppend(bf, "\x1b[7m", 4);
+					abAppend(bf, &sym, 1);
+					abAppend(bf, "\x1b[m", 3);
+					if (current_color != -1) {
+						char buf[16];
+						int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", current_color);
+						abAppend(bf, buf, clen);
+					}
+				} else if (hl[j] == HL_NORMAL) {
 					if (current_color != -1) {
 						abAppend(bf, "\x1b[39m", 5);
 						current_color = -1;
